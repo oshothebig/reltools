e15cd6590942088acb092f9d59ce2576ec0e2a84
Hari Gollapalli <hgollapalli@snaproute.com>
2016-06-28 12:21:12 -0700

    Added a few tools to make daily tasks easier

--- gittools/merges/fabfile.py
+++ gittools/merges/fabfile.py
@@ -6,35 +6,38 @@ from fabric.context_managers import settings
 
 env.use_ssh_config = True
 gSrRepos = ['asicd', 'l2', 'l3','config', 'utils','infra', 'flexSdk', 'apps', 'reltools', 'models', 'docs']
-#gSrRepos = ['l3', 'asicd' ]
-def mergeRepos (comp=None):
+def mergeRepos (branch=None, comp=None):
     global gSrRepos
     print 'Fetching Snaproute repositories dependencies....'
     srRepos = gSrRepos
     if comp != None :
-        srRepos = [comp]
+        srRepos = [comp] 
 
     for repo in srRepos:
-        cmds = ['git checkout -b pre_rel_1.x origin/pre_rel_1.x ',
+        cmds = ['git checkout -b %s origin/%s' %(branch, branch),
                 'git remote add upstream https://github.com/snaproute/%s.git' %(repo),
                 'git pull',
                 'git fetch upstream',
                 'git merge upstream/master',
                 'git push origin'
                 ]
-        local('git clone '+ 'https://github.com/snaproute/' + repo + '.git')
-        with lcd(repo):
-            for cmd in cmds:
-                local(cmd)
+        local('mkdir -p tmp')
+        with lcd('tmp'):
+            local('git clone '+ 'https://github.com/snaproute/' + repo + '.git')
+            with lcd(repo):
+                print '## Merging repo %s' %(repo)
+                for cmd in cmds:
+                    print 'Executing Command %s' %(cmd)
+                    local(cmd)
 
 def push(comp=None):
     global gSrRepos
-    print 'Pushing Snaproute repositories' 
     srRepos = gSrRepos
     if comp != None :
         srRepos = [comp]
 
     for repo in srRepos:
+        print 'Pushing repo %s'  %(repo)
         cmds = ['git push origin']
         with lcd(repo):
             for cmd in cmds:

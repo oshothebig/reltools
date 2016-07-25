c7afe9157522f860d4c8edcf3d1e8f7e82cab86a
Vikram Ragukumar <vragukumar@snaproute.com>
2016-07-25 11:08:04 -0700

    Add git lfs commands to fab file

--- fabfile.py
+++ fabfile.py
@@ -114,6 +114,15 @@ def setupSRRepos( gitProto = 'http' , comp = None):
                                     'git merge upstream/master']
                     for cmd in commandsToSync:
                         local(cmd)
+            LFSRepos = setupHandler().getLFSEnabledRepos()
+            if repo in LFSRepos:
+                with lcd(srcDir + repo):
+                    commandsToCheckout = [
+                            'git lfs fetch',
+                            'git lfs checkout'
+                            ]
+                    for cmd in commandsToCheckout:
+                        local(cmd)
 def installThrift():
     TMP_DIR = ".tmp"
     thriftVersion = '0.9.3'

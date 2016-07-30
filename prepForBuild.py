c8f046a7e20c3c1a63bf6197f76d5a2a618e2c95
Hari Gollapalli <hgollapalli@snaproute.com>
2016-07-29 17:59:45 -0700

    Added reltools to the repo to be updated

--- prepForBuild.py
+++ prepForBuild.py
@@ -9,15 +9,7 @@ if __name__ == '__main__':
 
     with open(SETUPFILE) as fd:
         info = json.load(fd)
-    repoList = info['PrivateRepos']
-
-    with open('pkgInfo.json') as pkg_info:
-	pk_info = json.load(pkg_info)
-    pkg_num = str(int(pk_info["build"]) + 1)
-    pk_info["build"] = str(int(pk_info["build"]) + 1)
-    
-    with open("pkgInfo.json", "w+") as fd_pkg:
-        json.dump(pk_info, fd_pkg, indent=4)
+    repoList = info['PrivateRepos'] + ['reltools']
 
     for repo in  repoList:
 	print "path of repo %s is : ~/git/snaproute/src/%s" %(repo, repo)
@@ -31,3 +23,12 @@ if __name__ == '__main__':
 		local ('git checkout master')
 		local ('git fetch upstream')
 		local ('git merge upstream/master')
+
+    with open('pkgInfo.json') as pkg_info:
+	pk_info = json.load(pkg_info)
+    pkg_num = str(int(pk_info["build"]) + 1)
+    pk_info["build"] = str(int(pk_info["build"]) + 1)
+    
+    with open("pkgInfo.json", "w+") as fd_pkg:
+        json.dump(pk_info, fd_pkg, indent=4)
+

88c4ae3cfc106f5dd8cf1debaf9652fb940902b4
ccordes-snaproute <ccordes@snaproute.com>
2016-08-27 20:14:25 -0700

    no longer auto-generating LACP/STP, code is now generated then edited with manual edits

--- codegentools/gencode.sh
+++ codegentools/gencode.sh
@@ -2,9 +2,9 @@
 cd $SR_CODE_BASE/reltools/codegentools/structs/
 source env.sh
 mkdir -p  $SR_CODE_BASE/reltools/codegentools/._genInfo
-pyang --plugindir `pwd` -f pybind  -o $SR_CODE_BASE/snaproute/src/models/objects/gen_stp.go $SR_CODE_BASE/snaproute/src/models/objects/yangmodel/stp/*.yang
+#pyang --plugindir `pwd` -f pybind  -o $SR_CODE_BASE/snaproute/src/models/objects/gen_stp.go $SR_CODE_BASE/snaproute/src/models/objects/yangmodel/stp/*.yang
 pyang --plugindir `pwd` -f pybind  -o $SR_CODE_BASE/snaproute/src/models/objects/gen_vxlan.go $SR_CODE_BASE/snaproute/src/models/objects/yangmodel/vxlan/*.yang
-pyang --plugindir `pwd` -f pybind  -o $SR_CODE_BASE/snaproute/src/models/objects/gen_lacp.go $SR_CODE_BASE/snaproute/src/models/objects/yangmodel/lacp/openconfig-if-aggregate.yang
+#pyang --plugindir `pwd` -f pybind  -o $SR_CODE_BASE/snaproute/src/models/objects/gen_lacp.go $SR_CODE_BASE/snaproute/src/models/objects/yangmodel/lacp/openconfig-if-aggregate.yang
 pyang --plugindir `pwd` -f pybind  -o $SR_CODE_BASE/snaproute/src/models/objects/gen_ldp.go $SR_CODE_BASE/snaproute/src/models/objects/yangmodel/ldp/ldp.yang
 cd $SR_CODE_BASE/reltools/codegentools/dbif/
 ./dbifGen.sh

1e421b12b05a62233b14df76fb87e5ec7e2d3cf8
Naga Praveen Illa <Naga Praveen Illa>
2016-09-01 08:32:47 -0700

    Add "actions" API to flexSDK

--- codegentools/apigen/generateApis.py
+++ codegentools/apigen/generateApis.py
@@ -3,6 +3,7 @@ import json
 from flexObject import FlexObject
 from flexConfigObject import FlexConfigObject
 from flexStateObject import FlexStateObject
+from flexActionObject import FlexActionObject
 
 class apiGenie (object) :
     def __init__ (self, outputDir, objDescriptors, attrDescriptionsDir) :
@@ -31,7 +32,12 @@ class apiGenie (object) :
                                                                   objInfo['multiplicity'],
                                                                   self.attrBase + objName + "Members.json"
                                                                   )
-
+                    elif 'x' in str(objInfo['access']):
+                        self.objDict[objName] = FlexActionObject (objName,
+                                                                  objInfo['access'],
+                                                                  objInfo['multiplicity'],
+                                                                  self.attrBase + objName + "Members.json"
+                                                                  )
     def writeApiCode(self) :
         filePath = ''
         basePath= os.getenv('SR_CODE_BASE')
@@ -62,7 +68,8 @@ if __name__ == '__main__':
     if not baseDir:
         print 'Environment variable SR_CODE_BASE is not set'
     
-    objDescriptors = [ baseDir + '/snaproute/src/models/objects/' + 'genObjectConfig.json', ]
+    objDescriptors = [ baseDir + '/snaproute/src/models/objects/' + 'genObjectConfig.json',
+                       baseDir + '/snaproute/src/models/actions/' + 'genObjectAction.json']
     attrDescriptorsLocation = baseDir+'/reltools/codegentools/._genInfo/'
     outputDir = baseDir+'/snaproute/src/flexSdk/py/'
     gen = apiGenie( outputDir, objDescriptors, attrDescriptorsLocation)

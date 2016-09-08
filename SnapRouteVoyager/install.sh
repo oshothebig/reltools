fa80328057b1bccdeb4c57c1e55bc2c44353e866
Vikram Ragukumar <vragukumar@snaproute.com>
2016-09-08 12:04:39 -0700

    Update cliPkg and installer script for Voyager

--- SnapRouteVoyager/install.sh
+++ SnapRouteVoyager/install.sh
@@ -1,63 +1,6 @@
 #!/bin/bash
 
 install_cli_pkg() {
-    python cliInstaller.py --path="${1}"
-}
-
-install_flexswitch_voyager() {
-        rpmName=${1}
-	fSRPM=$(ls | grep ${rpmName})
-        rpm -e --justdb --nodeps ${rpmName}
-        rm -rf /opt/flexswitch
-        rpm -Uvh ${fSRPM}
-#	if [ ${fSRPM} == "" ]
-#	then
-#		echo "Flexswitch RPM Not found"
-#		return
-#	fi
-#	fSRPMName=$(echo ${fSRPM} | sed -E 's/(.rpm)+$//')
-#	NewFSVersion=$(rpm -qilp ${fSRPM} | grep "Version" | awk -F': ' '{print $2}' | awk -F' ' '{print $1}' | head -1)
-#	OldFSVersion=$(rpm -qia flexswitch-voyager | grep "Version" | awk -F': ' '{print $2}' | awk -F' ' '{print $1}')
-#	if [ "${OldFSVersion}" != "" ]
-#	then
-#		if [[ "${OldFSVersion}" < "${NewFSVersion}" ]]
-#		then
-#			echo "Old Version of Flexswitch installed, Upgrading it"
-#			rpm -Uvh ${fSRPM}
-#			if [ $? -eq 0 ]
-#			then
-#				echo "Successfully installed ${fSRPMName}"
-#			else
-#				echo "Unable to install ${fSRPM}. Please install it manually"
-#			fi
-#		else
-#			echo "Higher or Same version of FLexswitch is already installed"
-#		fi
-#	else
-#		echo "installing ${fSRPMName}"
-#		rpm -Uvh ${fSRPM}
-#		if [ $? -eq 0 ]
-#		then
-#			echo "Successfully installed ${fSRPMName}"
-#		else
-#			echo "Unable to install ${fSRPM}. Please install it manually"
-#		fi
-#	fi
-}
-
-install_flexswitch() {
-	echo "Not supported"
-}
-
-if [ "${1}" == "flexswitch-voyager" ]
-then
-        /opt/flexswitch/flexswitch --op=stop
-        /etc/init.d/redis stop
-        /etc/init.d/rsyslog stop
-	./install_centos_dependency.sh
-	ln -sf /etc/init.d/redis /etc/rc3.d/S80redis
-        /etc/init.d/redis start
-        /etc/init.d/rsyslog start
 	which python2.7 > /dev/null
         if [ $? -ne 0 ]
         then
@@ -72,13 +15,41 @@ then
                 done
                 printf "] done!"
         fi
-	install_flexswitch_voyager flexswitch-voyager
-	chkconfig --level 345 flexswitch on
-        /opt/flexswitch/flexswitch --op=start
-    	install_cli_pkg "${2}"
+    python cliInstaller.py --path="${1}"
+}
+
+install_flexswitch_voyager() {
+    rpmName=${1}
+	fSRPM=$(ls | grep ${rpmName})
+    rpm -e --justdb --nodeps ${rpmName}
+    rm -rf /opt/flexswitch
+    rm -rf /etc/flexswitch
+	rm -rf /etc/init.d/flexswitch*
+    rpm -Uvh ${fSRPM}
+}
+
+install_flexswitch() {
+	echo "Not supported"
+}
+
+if [ "${1}" == "flexswitch-voyager" ]
+then
+    /opt/flexswitch/flexswitch --op=stop
+    redis-cli flushdb
+    /etc/init.d/redis stop
+    ./install_centos_dependency.sh
+    ln -sf /etc/init.d/redis /etc/rc3.d/S80redis
+    /etc/init.d/redis start
+    /etc/init.d/rsyslog start
+    install_flexswitch_voyager flexswitch-voyager
+    chkconfig --level 345 flexswitch on
+    /opt/flexswitch/flexswitch --op=start
 elif [ "${1}" == "flexswitch" ]
 then
 	install_flexswitch
+elif [ "${1}" == "flexswitchCli" ]
+then
+    install_cli_pkg "${2}"
 else
 	echo "Usage: ./install.sh <Platform>"
 	echo "Platform: flexswitch, flexswitch-voyager"
